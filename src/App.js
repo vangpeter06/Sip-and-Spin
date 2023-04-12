caae5ae05b9ceb4d12bc5c12b2f463985e2ca840
Peter Vang <vang_peter06@yahoo.com>
2023-04-12 15:40:38 -0700

    add all directory to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Review from "./components/reviews/Review";
+import Menu from "./components/menu/Menu";
+import Nav from "./components/nav/Nav";
+import Photos from "./photos/Photos";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <About />
+      <Review />
+      <Menu />
+      <Nav />
+      <Photos />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
