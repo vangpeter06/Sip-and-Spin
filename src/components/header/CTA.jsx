50342da0160e297a98ea77d2d65c7e16e6cd2410
Peter Vang <vang_peter06@yahoo.com>
2023-04-12 15:36:36 -0700

    add CTA to header

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href="tel:503-328-8289" CALL className="btn">
+        503-328-8289
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Message
+      </a>
+      {/* <a href="https://www.google.com/maps/dir//Sip+%26+Spin/@45.4923225,-122.5582524,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x54959f00f8b4e36b:0x8db0f1e6e1fa9bb3!2m2!1d-122.4881831!2d45.492412" >
+        <p>17102 Southeast Powell Boulevard</p>
+        <p>Portland, OR 97236</p>
+      </a> */}
+    </div>
+  );
+};
+
+export default CTA;
